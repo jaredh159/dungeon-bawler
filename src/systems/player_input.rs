27e021a5f377c228a7df114e259e54dd6fd87294
Jared Henderson <jaredh159@users.noreply.github.com>
2023-12-09 13:38:03 -0500

    WantsToMove intents for movement

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,15 +1,15 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
   world: &mut SubWorld,
-  #[resource] map: &Map,
+  commands: &mut CommandBuffer,
   #[resource] key: &Option<VirtualKeyCode>,
-  #[resource] camera: &mut Camera,
   #[resource] turn_state: &mut TurnState,
 ) {
+  let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
   if let Some(key) = key {
     let delta = match key {
       VirtualKeyCode::Left | VirtualKeyCode::H => Point::new(-1, 0),
@@ -18,16 +18,10 @@ pub fn player_input(
       VirtualKeyCode::Down | VirtualKeyCode::J => Point::new(0, 1),
       _ => Point::zero(),
     };
-    if delta.x != 0 || delta.y != 0 {
-      let mut players = <&mut Point>::query().filter(component::<Player>());
-      players.iter_mut(world).for_each(|pos| {
-        let destination = *pos + delta;
-        if map.can_enter_tile(destination) {
-          *pos = destination;
-          camera.on_player_move(destination);
-          *turn_state = TurnState::PlayerTurn;
-        }
-      });
-    }
+    players.iter_mut(world).for_each(|(entity, pos)| {
+      let destination = *pos + delta;
+      commands.push(((), WantsToMove { entity: *entity, destination }));
+    });
+    *turn_state = TurnState::PlayerTurn;
   }
 }
