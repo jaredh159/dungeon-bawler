c69e080182b2bed1db4f2deff35277e74d13a3c2
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-16 16:30:57 -0400

    randomize prefabs

--- src/map_builder/prefab.rs
+++ src/map_builder/prefab.rs
@@ -1,10 +1,12 @@
 use crate::prelude::*; // `*` means "all things, or everything"
 
-const COOL_LOOKIN_ROOM: (&str, i32, i32) = (
+const COOL_LOOKIN_ROOM_1: (&str, i32, i32) = (
   "
 ............
-...######...
-...#....#...
+......##....
+.....#.#....
+....#..#....
+....#...#...
 ...#....#...
 ...#....###.
 ...#.....M..
@@ -17,7 +19,51 @@ const COOL_LOOKIN_ROOM: (&str, i32, i32) = (
 ............
 ",
   12,
-  13,
+  15,
+);
+
+const COOL_LOOKIN_ROOM_2: (&str, i32, i32) = (
+  "
+............
+.####.#####.
+.####.#####.
+.####.#####.
+.####.#####.
+.####.#####.
+.####.#####.
+.####..####.
+.####...###.
+.###....###.
+.##..M..###.
+.##......##.
+.#........#.
+.##########.
+............
+",
+  12,
+  15,
+);
+
+const COOL_LOOKIN_ROOM_3: (&str, i32, i32) = (
+  "
+............
+.####.#####.
+.####..####.
+.#####..###.
+.######..##.
+.#######..#.
+.######..##.
+.#####..###.
+.####..####.
+.###..#####.
+.##..######.
+.#..#######.
+.#M.#######.
+.##########.
+............
+",
+  12,
+  15,
 );
 
 pub fn apply_prefab(mb: &mut MapBuilder, rng: &mut RandomNumberGenerator) {
@@ -31,13 +77,19 @@ pub fn apply_prefab(mb: &mut MapBuilder, rng: &mut RandomNumberGenerator) {
     1024.0,
   );
 
+  let prefab = match rng.range(0, 3) {
+    0 => COOL_LOOKIN_ROOM_1,
+    1 => COOL_LOOKIN_ROOM_2,
+    _ => COOL_LOOKIN_ROOM_3,
+  };
+
   let mut attempts = 0;
   while placement.is_none() && attempts < 10 {
     let possible_placement_rect = Rect::with_size(
-      rng.range(0, SCREEN_WIDTH - COOL_LOOKIN_ROOM.1),
-      rng.range(0, SCREEN_HEIGHT - COOL_LOOKIN_ROOM.2),
-      COOL_LOOKIN_ROOM.1,
-      COOL_LOOKIN_ROOM.2,
+      rng.range(0, SCREEN_WIDTH - prefab.1),
+      rng.range(0, SCREEN_HEIGHT - prefab.2),
+      prefab.1,
+      prefab.2,
     );
     let mut can_place = false;
     possible_placement_rect.for_each(|pt| {
@@ -61,15 +113,15 @@ pub fn apply_prefab(mb: &mut MapBuilder, rng: &mut RandomNumberGenerator) {
   println!("placed: {}", placement.is_some());
 
   if let Some(placement) = placement {
-    let vec_of_chars: Vec<char> = COOL_LOOKIN_ROOM
+    let vec_of_chars: Vec<char> = prefab
       .0
       .chars()
       .filter(|a| *a != '\r' && *a != '\n')
       .collect();
 
     let mut i = 0;
-    for ty in placement.y..placement.y + COOL_LOOKIN_ROOM.2 {
-      for tx in placement.x..placement.x + COOL_LOOKIN_ROOM.1 {
+    for ty in placement.y..placement.y + prefab.2 {
+      for tx in placement.x..placement.x + prefab.1 {
         let idx = map_index(tx, ty);
         let c = vec_of_chars[i];
         match c {
