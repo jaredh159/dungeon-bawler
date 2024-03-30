718fb79e17f36d5fab201fc387c0e5316f555b13
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-30 14:28:04 -0400

    show inventory

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -3,6 +3,9 @@ use crate::prelude::*;
 #[system]
 #[read_component(Health)]
 #[read_component(Player)]
+#[read_component(Item)]
+#[read_component(Carried)]
+#[read_component(Name)]
 pub fn hud(world: &SubWorld) {
   let mut health_query = <&Health>::query().filter(component::<Player>());
   let player_health = health_query.iter(world).next().unwrap();
@@ -33,5 +36,27 @@ pub fn hud(world: &SubWorld) {
     ColorPair::new(GREEN, DARK_RED),
   );
 
+  let player = <(Entity, &Player)>::query()
+    .iter(world)
+    .map(|(entity, _)| *entity)
+    .next()
+    .unwrap();
+
+  let mut item_query = <(&Item, &Name, &Carried)>::query();
+  let mut y = 3;
+  item_query
+    .iter(world)
+    .filter(|(_, _, carried)| carried.0 == player)
+    .for_each(|(_, name, _)| {
+      draw_batch.print(Point::new(3, y), format!("{} : {}", y - 2, &name.0));
+      y += 1;
+    });
+  if y > 3 {
+    draw_batch.print_color(
+      Point::new(3, 2),
+      "Items carried",
+      ColorPair::new(YELLOW, BLACK),
+    );
+  }
   draw_batch.submit(10000).expect("Bachelor error");
 }
